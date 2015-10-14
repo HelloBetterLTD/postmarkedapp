16c1783ab60b8fe095254cc6499c913ece7b5d06
Nivanka Fonseka <nivanka@silverstripers.com>
2015-10-14 14:07:12 +0530

    upbeat selector field accept more items

--- code/Helpers/ObjectSelectorField.php
+++ code/Helpers/ObjectSelectorField.php
@@ -62,9 +62,20 @@ class ObjectSelectorField extends FormField {
 		return 'ID';
 	}
 
+	function getDisplayFieldOptions(){
+		if($this->hasMultiSortOptions()){
+			return implode('__', $this->getDisplayTitleField());
+		}
+		return $this->getDisplayTitleField();
+	}
+
+	function hasMultiSortOptions(){
+		return is_array($this->getDisplayTitleField());
+	}
+
 	function Link($action = null){
 		if($this->customLink){
-			return Director::baseURL() . 'ObjectSelectorField_Controller/find/?sourceObject=' . $this->sourceObject . '&displayField=' . $this->getDisplayTitleField();
+			return Director::baseURL() . 'ObjectSelectorField_Controller/find/?sourceObject=' . $this->sourceObject . '&displayField=' . $this->getDisplayFieldOptions() . '&multi=' . $this->hasMultiSortOptions();
 		}
 		return parent::Link($action);
 	}
@@ -103,9 +114,31 @@ class ObjectSelectorField extends FormField {
 	function find(){
 		$arr = array();
 		if(isset($_GET['filter']) && $this->sourceObject){
-			$list = DataList::create($this->sourceObject)->filter($this->displayField . ':PartialMatch', $_GET['filter']);
+			$list = DataList::create($this->sourceObject);
+			if($this->hasMultiSortOptions()){
+				$arrFilters = array();
+				foreach($this->displayField as $strField){
+					$arrFilters[$strField . ':PartialMatch'] = $_GET['filter'];
+				}
+				$list = $list->filterAny($arrFilters);
+			}
+			else{
+				$list = $list->filter($this->displayField . ':PartialMatch', $_GET['filter']);
+			}
+
+
 			foreach($list as $item){
-				$arr[$item->ID]  = $item->getField($this->displayField);
+				if($this->hasMultiSortOptions()){
+					$arrVal = array();
+					foreach($this->displayField as $strField){
+						$arrVal[] = $item->getField($strField);
+ 					}
+					$arr[$item->ID] = implode(' | ', $arrVal);
+				}
+				else{
+					$arr[$item->ID]  = $item->getField($this->displayField);
+				}
+
 			}
 		}
 		return Convert::array2json($arr);
@@ -119,12 +152,38 @@ class ObjectSelectorField_Controller extends Controller {
 		'find'
 	);
 
+	public function hasMultiSortOptions(){
+		return isset($_GET['multi']) && $_GET['multi'] == 1;
+	}
+
 	function find(){
 		$arr = array();
 		if(isset($_GET['filter']) && isset($_GET['sourceObject']) && isset($_GET['displayField'])){
-			$list = DataList::create($_GET['sourceObject'])->filter($_GET['displayField'] . ':PartialMatch', $_GET['filter']);
+
+			$list = DataList::create($_GET['sourceObject']);
+			if($this->hasMultiSortOptions()){
+				$arrFilters = array();
+				foreach(explode('__', $_GET['displayField']) as $strField){
+					$arrFilters[$strField . ':PartialMatch'] = $_GET['filter'];
+				}
+				$list = $list->filterAny($arrFilters);
+
+			}
+			else{
+				$list = $list->filter($_GET['displayField'] . ':PartialMatch', $_GET['filter']);
+			}
+
 			foreach($list as $item){
-				$arr[$item->ID]  = $item->getField($_GET['displayField']);
+				if($this->hasMultiSortOptions()){
+					$arrVal = array();
+					foreach(explode('__', $_GET['displayField']) as $strField){
+						$arrVal[] = $item->getField($strField);
+					}
+					$arr[$item->ID] = implode(' | ', $arrVal);
+				}
+				else{
+					$arr[$item->ID]  = $item->getField($_GET['displayField']);
+				}
 			}
 		}
 		return Convert::array2json($arr);
